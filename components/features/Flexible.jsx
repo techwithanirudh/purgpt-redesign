73cdca0634b85601c23a7ec8176d2770a4544c2c
Sidd_ <sdmdevelopment3@gmail.com>
2023-07-11 13:36:54 -0700

    now mobile happy

--- components/features/Flexible.jsx
+++ components/features/Flexible.jsx
@@ -2,11 +2,11 @@ import React, { useState, useEffect } from "react";
 
 export default function Flexible() {
   return (
-    <div className="flex justify-center items-center h-full -mt-10">
-      <p className="text-3xl">
+    <div className="flex justify-center items-center h-full md:-mt-10">
+      <p className="md:text-3xl">
         "Be {" "}
         <span className="scrolling-words-container bg-white/10 px-4 -mb-2 rounded-md inline-block relative overflow-hidden">
-          <span className="inline-block text-left animate-scrolling-words">
+          <span className="inline-block text-left animate-scrolling-words-mobile md:animate-scrolling-words">
             a fun coder <br />
             a mathematician <br />
             a nerdy flautist <br />
